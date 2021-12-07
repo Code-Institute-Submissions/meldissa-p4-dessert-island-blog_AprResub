2eedeeed8986ffaf48224304833b1a7ba10e2c45
meldissa <melanie_9401@yahoo.co.uk>
2021-12-07 20:16:11 +0000

    Update templates and static files

--- dessertisland/settings.py
+++ dessertisland/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['p4-dessert-island.herokuapp.com', 'localhost']
 
 
