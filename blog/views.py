1405db233fc1ff9fb3f48fa0cd411d9b1324a2c5
meldissa <melanie_9401@yahoo.co.uk>
2021-12-10 21:03:05 +0000

    Add search functionality

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,16 @@ def about(request):
     return render(request, 'about.html')
 
 
+def search(request):
+    """ A view to return the search page """
+    if request.method == "POST":
+        searched = request.POST.get('searched', None)
+        results = Post.objects.filter(title__contains=searched)
+        return render(request, 'search.html', {'searched': searched, 'results': results})
+    else:
+        return render(request, 'search.html')
+
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
