a7c976f2651cd5a548caafa60691171a857d9e40
meldissa <melanie_9401@yahoo.co.uk>
2022-01-12 21:46:45 +0000

    Update tests for views and models

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,20 +1,26 @@
 from django.test import TestCase
 from blog.models import Post, Comment
+from django.contrib.auth.models import User
 
 
 class TestModels(TestCase):
 
     def test_post_status_defaults_to_draft(self):
+        user = User.objects.create(username='testname')
         recipe = Post.objects.create(
             title='recipe1',
-            author='user1'
+            author=user
         )
         self.assertFalse(recipe.status)
-    
+
     def test_comment_approved_defaults_to_false(self):
+        user = User.objects.create(username='testname')
+        recipe = Post.objects.create(
+            title='recipe1',
+            author=user
+        )
         comment = Comment.objects.create(
-            name='user1',
+            post=recipe,
             body='recipe comment',
-            post_id='1'
         )
         self.assertFalse(comment.approved)
--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,34 +1,45 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from blog.models import Post, Comment
+from django.contrib.auth.models import User
 
 
 class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.home_url = reverse('home')
+        self.about_url = reverse('about')
+        self.search_url = reverse('search')
+        self.post_detail_url = reverse('post_detail', args=['recipe1'])
+        self.post_like_url = reverse('post_like', args=['recipe1'])
+        self.user = User.objects.create(username='testname')
+        self.recipe = Post.objects.create(
+            title='recipe1',
+            author=self.user
+        )
 
     def test_post_list_GET(self):
-        response = self.client.get(reverse('home'))
+        response = self.client.get(self.home_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_post_detail_GET(self):
-        response = self.client.get(reverse('post_detail', args=['recipe1']))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_detail.html')
-
-    def test_post_like_GET(self):
-        response = self.client.get(reverse('post_like', args=['recipe1']))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_detail.html')
-
     def test_about_GET(self):
-        response = self.client.get(reverse('about'))
+        response = self.client.get(self.about_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'about.html')
 
     def test_search_GET(self):
-        response = self.client.get(reverse('search'))
+        response = self.client.get(self.search_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'search.html')
+
+    def test_post_detail_POST(self):
+        response = self.client.post(self.post_detail_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_like_POST(self):
+        response = self.client.post(self.post_like_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_detail.html')
